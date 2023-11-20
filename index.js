eb3a38ad74e8d15d6d06bdffdc456d246f3afc1f
Adeel Durrani <jeoydurrani2003@gmail.com>
2023-11-20 08:19:39 -0500

    Added Bouncy Effect

--- index.js
+++ index.js
@@ -8,24 +8,52 @@ const toggleDarkMode = () => {
 themeButton.addEventListener("click", toggleDarkMode);
 //
 
+//animation
+let animation = {
+  revealDistance : 150,
+  initialOpacity : 0,
+  transitionDelay : 0,
+  transitionDuration : '2s',
+  transitionProperty : 'all',
+  transitionTimingFunction : 'ease'
+}
+
+//reveals
+let revealableContainers = document.querySelectorAll('.revealable');
+
+let reveal = () => {
+  for (let i =0; i < revealableContainers.length; i++) {
+    let windoHeight = window.innerHeight;
+    let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+    if (topOfRevealableContainer < windoHeight - animation.revealDistance) {
+      revealableContainers[i].classList.add('active');
+  } else {
+        revealableContainers[i].classList.remove('active');
+  }
+}
+}
+
+//reveal event listener
+window.addEventListener('scroll', reveal);
+
 // Add your query for the sign now button here
 let signNowButton = document.getElementById('sign-now-button');
 
 // Initialize the count of signatures
 let count = 3;
 
-const addSignature = () => {
+const addSignature = (person) => {
+  
     // Retrieve input values
     let name = document.getElementById('name').value;
     let hometown = document.getElementById('hometown').value;
 
     // Create a new paragraph element for the signature
-    let newSignature = document.createElement('p');
-    newSignature.textContent = `🖊️ ${name} from ${hometown} supports this.`;
+  let newSignature = document.createElement('p');
+  newSignature.textContent = `🖊️ ${person.name} from ${person.hometown} supports this.`;
 
-    // Find the container where the signatures are displayed
-    let signaturesContainer = document.querySelector('.signatures');
-    signaturesContainer.appendChild(newSignature);
+  let signaturesContainer = document.querySelector('.signatures');
+  signaturesContainer.appendChild(newSignature);
 
     // Update the counter
     updateCounter();
@@ -51,9 +79,13 @@ const updateCounter = () => {
 }
 
 const validateForm = () => {
-
   let containsErrors = false;
-
+  let person = {
+      name: document.getElementById('name').value,
+      hometown: document.getElementById('hometown').value,
+      email: document.getElementById('email').value
+  };
+  
   var petitionInputs = document.getElementById("sign-petition").elements;
   // TODO: Loop through all inputs
   for (let i = 0; i < petitionInputs.length; i++) {
@@ -67,8 +99,9 @@ const validateForm = () => {
     }
   }
   // TODO: Call addSignature() and clear fields if no errors
-  if (!containsErrors) {
-    addSignature();
+    if (!containsErrors) {
+    addSignature(person);
+    toggleModal(person);
     for (let i = 0; i < petitionInputs.length; i++) {
       petitionInputs[i].value = "";
       containsErrors = false;
@@ -82,6 +115,29 @@ const validateForm = () => {
   } else {
     email.classList.remove('error');
   }
+}
+
+let scaleFactor = 1;
+let modalImage = document.getElementById('thanks-modal').querySelector('img');
+
+const toggleModal = (person) => {
+    let modal = document.getElementById('thanks-modal');
+    let modalContent = document.getElementById('thanks-modal-content');
+
+    modal.style.display = 'flex';
+    modalContent.textContent = `Thank you ${person.name}!`;
+
+    let intervalId = setInterval(scaleImage, 500);
+
+    setTimeout(() => {
+        modal.style.display = 'none';
+        clearInterval(intervalId);
+    }, 4000);
+}
+
+const scaleImage = () => {
+    scaleFactor = scaleFactor === 1 ? 0.8 : 1;
+    modalImage.style.transform = `scale(${scaleFactor})`;
 }
 
 signNowButton.addEventListener('click', validateForm);
